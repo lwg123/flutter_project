08f51de83f582fef572e5659988a33135be633d3
liweiguang@duia.com <weiguang.li@changhong.com>
2019-03-28 17:23:24 +0800

    添加分类

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../service/service_metnod.dart';
 import 'dart:convert';
+import '../model/category.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 
 class CategoryPage extends StatefulWidget {
@@ -9,22 +11,137 @@ class CategoryPage extends StatefulWidget {
 }
 
 class _CategoryPageState extends State<CategoryPage> {
+
   @override
   Widget build(BuildContext context) {
-    _getData();
-    return Container(
-      child: Center(
-        child: Text('分类页面'),
+    return Scaffold(
+      appBar: AppBar(title: Text('商品分类'),),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            LeftCategoryNav(),
+            Column(
+              children: <Widget>[
+                RightCategoryNav(),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
 
-  void _getData() async{
+}
+
+// 左侧导航菜单
+class LeftCategoryNav extends StatefulWidget {
+  @override
+  _LeftCategoryNavState createState() => _LeftCategoryNavState();
+}
+
+class _LeftCategoryNavState extends State<LeftCategoryNav> {
+  List list = [];
+
+  @override
+  void initState() {
+    _getCategory();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenUtil().setWidth(180),
+      decoration: BoxDecoration(
+        border: Border(
+          right: BorderSide(width: 1,color: Colors.black12)
+        )
+      ),
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context,index){
+          return _leftInkWell(index);
+        },
+      ),
+    );
+  }
+
+  Widget _leftInkWell(int index){
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        height: ScreenUtil().setHeight(100),
+        padding: EdgeInsets.only(left: 10,top: 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(width: 1,color: Colors.black12)
+          )
+        ),
+        child: Text(list[index].mallCategoryName,style: TextStyle(fontSize: ScreenUtil().setSp(28)),),
+      ),
+    );
+  }
+
+
+  void _getCategory() async{
     await request('getCategory').then((val){
-      var data =json.decode(val);
-      print(data);
+      var data =json.decode(val.toString());
+      CategoryModel category =CategoryModel.fromJson(data);
+      
+      setState(() {
+        list = category.data;
+      });
     });
   }
 
+  
 }
 
+
+// 右侧小类类别
+class RightCategoryNav extends StatefulWidget {
+  @override
+  _RightCategoryNavState createState() => _RightCategoryNavState();
+}
+
+class _RightCategoryNavState extends State<RightCategoryNav> {
+
+  List list = ['名酒','宝丰','北京二锅头','舍得','五粮液','茅台','泸州老窖'];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        height: ScreenUtil().setHeight(80),
+        width: ScreenUtil().setWidth(570),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(width: 1,color: Colors.black12)
+          )
+        ),
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: list.length,
+          itemBuilder: (context,index){
+            return _rightInkWell(list[index]);
+          },
+        )
+      ),
+    );
+  }
+
+  Widget _rightInkWell(String item){
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        padding: EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 10.0),
+        child: Text(
+          item,
+          style:TextStyle(fontSize:ScreenUtil().setSp(28)),
+        ),
+      ),
+    );
+  }
+
+}
