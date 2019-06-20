0069b275e195760a6a289a50faf954d9aff7195a
weiguang <liweiguang@duia.com>
2019-06-20 16:48:00 +0800

    update Code

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -8,17 +8,40 @@ class DetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    _getBackInfo(context);
-    return Container(
-      child: Center(
-        child: Text('商品ID为:${goodsId}'),
+    
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: (){
+            Navigator.pop(context);
+          },
+        ),
+        title: Text('商品详细页'),
       ),
+      body: FutureBuilder(
+        future: _getBackInfo(context),
+        builder: (context, snapshot){
+          if (snapshot.hasData) {
+            return Container(
+               child: Column(
+                 children: <Widget>[
+                   Text('商品ID：goodsId')
+                 ],
+               ), 
+            );
+          } else {
+            return Text('加载中');
+          }
+        },
+      ),
+
     );
   }
 
-  void _getBackInfo(BuildContext context) async {
+  Future _getBackInfo(BuildContext context) async {
     await Provide.value<DetailsInfoProvide>(context).getGoodsInfo(goodsId);
-    print('加载完成。。。');
+    return '完成加载';
   }
 
 }
